fd3d3e6b9c6e75e646cde9c71a09c916ab6c6bfa
Marcel Eichner <love@ephigenia.de>
2026-03-16 21:10:55 +0100

    chore: font rendering

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 var assets embed.FS
 
 func main() {
-	ebiten.SetWindowSize(game.ScreenW, game.ScreenH)
+	scale := 2
+	ebiten.SetWindowSize(game.ScreenW*scale, game.ScreenH*scale)
 	ebiten.SetWindowTitle("Dungeon Crawler")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	g := game.New(assets)
